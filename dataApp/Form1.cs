8b2036a0e1418d33cf722b780f1389bcd2bb8208
unknown <zhangroc@SHACNG107WLGF.ads.autodesk.com>
2012-01-30 10:59:24 +0800

    update the convert Double character to half character when user input the digital in double mode

--- dataApp/Form1.cs
+++ dataApp/Form1.cs
@@ -901,7 +901,12 @@ namespace app
 
         private void Text_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if( (int)e.KeyChar >= 65296 && (int)e.KeyChar <=65305)
+            if ((int)e.KeyChar == 12288)
+            {
+                e.KeyChar = (char)32;
+            }
+
+            if ((int)e.KeyChar >= 65280 && (int)e.KeyChar <= 65375)
             {
                 e.KeyChar =(char)((int)e.KeyChar -65248);
             }
@@ -912,7 +917,31 @@ namespace app
             
         }
 
-
+        //// <summary>
+        /// 转半角的函数(DBC case)
+        /// </summary>
+        /// <param name="input">任意字符串</param>
+        /// <returns>半角字符串</returns>
+        ///<remarks>
+        ///全角空格为12288，半角空格为32
+        ///其他字符半角(33-126)与全角(65281-65374)的对应关系是：均相差65248
+        ///</remarks> 
+        private string DoubleToHalf(string dstring)
+        {
+            char[] c = dstring.ToCharArray();
+            for (int i = 0; i < c.Length; i++)
+            {
+                if ( c[i] == 12288)
+                {
+                    c[i] = (char)32;
+                }
+                else if ( c[i] > 65280 && c[i] < 65375)
+                {
+                    c[i] = (char)((int)c[i] - 65248);
+                }
+            }
+            return new string(c);
+        }
 
 
 
